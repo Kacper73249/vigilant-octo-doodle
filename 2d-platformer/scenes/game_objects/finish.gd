85552ad7005d69c4ede56bc5b00c2f5b6d74407b
Kacper73249 <gdx173249@student.gdansk.merito.pl>
2026-04-12 18:17:53 +0200

    Bugfix + Warunek zwycięstwa
    Naprawiłem buga, w którym na poziome 2 otrzymanie obrażeń powoduje crasha. Dodałem również warunek, że puchar nie przenosi do następnego poziomu dopóki, gracz nie podniesie wszystkich owocków, sygnowane kolorem czerwonym zamiast złotego/żółtego

--- 2d-platformer/scenes/game_objects/finish.gd
+++ 2d-platformer/scenes/game_objects/finish.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
 @export var target_level : PackedScene
+@onready var game_manager: Node = %GameManager
 
+func _process(delta):
+	if game_manager.points < 12:
+		$Sprite2D.modulate = Color(1, 0, 0)
+		$CollisionShape2D.disabled = true
+	else:
+		$Sprite2D.modulate = Color(1, 1, 1)
+		$CollisionShape2D.disabled = false
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
 		get_tree().change_scene_to_packed(target_level)
